c5dd7b9065444279d8554a3f3a648b77482316da
linhne94 <linhnqde160572@fpt.edu.vn>
2024-09-09 16:22:00 +0700

    DONE verify account, forgot password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,15 @@
 const responseHandler = require("../middlewares/responseHandler");
-var { login, register } = require("../services/authService");
+var {
+  login,
+  register,
+  sendMailVeriry,
+  verifyAccount,
+  forgotPassword,
+  resetPassword,
+  verifyTokenRs,
+} = require("../services/authService");
 
+// authenticate
 const loginController = async (req, res, next) => {
   const loginResult = await login(req.body);
   // console.log(loginResult);
@@ -30,8 +39,58 @@ const logoutController = async (req, res, next) => {
   responseHandler(200, null, "Logout successful")(req, res, next);
 };
 
+//Verify account
+const sendMailVerifyController = async (req, res, next) => {
+  const email = req.body.email;
+  // console.log("controller:", email);
+  const id = req.user.id;
+  const result = await sendMailVeriry(email, id);
+
+  responseHandler(result.status, null, result.message)(req, res, next);
+};
+
+const verifyAccountController = async (req, res, next) => {
+  const token = req.params.token;
+  // console.log("test: ", token);
+  const result = await verifyAccount(token);
+
+  responseHandler(result.status, null, result.message)(req, res, next);
+};
+
+// Forgot pasword
+const verifyTokenRsController = async (req, res, next) => {
+  const token = req.params.token;
+  const result = await verifyTokenRs(token);
+
+  responseHandler(result.status, result.data, result.message)(req, res, next);
+};
+
+const sendMailForgotPass = async (req, res, next) => {
+  const email = req.body.email;
+  const result = await forgotPassword(email);
+
+  responseHandler(result.status, null, result.message)(req, res, next);
+};
+
+const resetPasswordController = async (req, res, next) => {
+  const data = req.body;
+
+  const result = await resetPassword(
+    data.email,
+    data.userId,
+    data.newPassword,
+    data.confirmPassword
+  );
+  responseHandler(result.status, null, result.message)(req, res, next);
+};
+
 module.exports = {
   loginController,
   registerController,
   logoutController,
+  sendMailVerifyController,
+  verifyAccountController,
+  sendMailForgotPass,
+  resetPasswordController,
+  verifyTokenRsController,
 };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,6 +3,11 @@ const {
   loginController,
   registerController,
   logoutController,
+  sendMailVerifyController,
+  verifyAccountController,
+  verifyTokenRsController,
+  sendMailForgotPass,
+  resetPasswordController,
   // getTestController,
 } = require("../controllers/authController");
 const { verifyUser } = require("../middlewares/verifyToken");
@@ -12,6 +17,13 @@ const authRouter = express.Router();
 authRouter.post("/login", loginController);
 authRouter.post("/register", registerController);
 authRouter.get("/logout", verifyUser, logoutController);
+// verify email
+authRouter.post("/mail-verify", verifyUser, sendMailVerifyController);
+authRouter.get("/verify-account/:token", verifyAccountController);
+// forgot pasword
+authRouter.get("/verify-token/:token", verifyTokenRsController);
+authRouter.post("/forgot-password", sendMailForgotPass);
+authRouter.post("/reset-password", resetPasswordController);
 
 // authRouter.get("/", getTestController);
 
