06ce3f563944277031c19bd4fb4f02a3b2f4bab0
linhne94 <linhnqde160572@fpt.edu.vn>
2024-09-10 13:43:37 +0700

    inprogress - follow, list followed

--- server/migrations/20240909091702-create-table-subscribe.js
+++ server/migrations/20240909091702-create-table-subscribe.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("subcribes", {
+    await queryInterface.createTable("subscribes", {
       userId: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -40,6 +40,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("subcribes");
+    await queryInterface.dropTable("subscribes");
   },
 };
