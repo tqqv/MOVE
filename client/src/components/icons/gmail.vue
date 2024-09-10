80bdbe14a3d02af11aa2a9e84bfeb04df58682ac
The Hoang <thehoang1722@gmail.com>
2024-09-10 13:35:39 +0700

    link navbar vs login

--- client/src/components/icons/gmail.vue
+++ client/src/components/icons/gmail.vue
@@ -7,16 +7,14 @@
   });
 </script>
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="24.604" height="25" viewBox="0 0 24.604 25">
-    <g id="Logo_-_Google" data-name="Logo - Google" transform="translate(0.629 0.518)">
+  <svg xmlns="http://www.w3.org/2000/svg" width="23.525" height="24" viewBox="0 0 23.525 24">
+    <g id="Logo_-_Google" data-name="Logo - Google" transform="translate(0.05 0.018)">
       <path
         id="Path_542"
         data-name="Path 542"
         d="M283.626,225.538a14.113,14.113,0,0,0-.207-2.438H272.1v4.621h6.481a5.554,5.554,0,0,1-2.4,3.646v3h3.867A11.719,11.719,0,0,0,283.626,225.538Z"
         transform="translate(-260.15 -213.281)"
         fill="#4285f4"
-        stroke="#707070"
-        stroke-width="1"
       />
       <path
         id="Path_543"
@@ -24,8 +22,6 @@
         d="M39.623,334a11.487,11.487,0,0,0,7.954-2.9l-3.867-3A7.278,7.278,0,0,1,32.89,324.3H28.9v3.091A12,12,0,0,0,39.623,334Z"
         transform="translate(-27.674 -310.019)"
         fill="#34a853"
-        stroke="#707070"
-        stroke-width="1"
       />
       <path
         id="Path_544"
@@ -33,8 +29,6 @@
         d="M5.212,157.685a7.188,7.188,0,0,1,0-4.594V150H1.226a12.01,12.01,0,0,0,0,10.776Z"
         transform="translate(0 -143.404)"
         fill="#fbbc04"
-        stroke="#707070"
-        stroke-width="1"
       />
       <path
         id="Path_545"
@@ -42,8 +36,6 @@
         d="M39.623,4.731a6.521,6.521,0,0,1,4.6,1.8L47.652,3.1A11.533,11.533,0,0,0,39.623-.018,12,12,0,0,0,28.9,6.6l3.986,3.091A7.177,7.177,0,0,1,39.623,4.731Z"
         transform="translate(-27.674 0)"
         fill="#ea4335"
-        stroke="#707070"
-        stroke-width="1"
       />
     </g>
   </svg>
