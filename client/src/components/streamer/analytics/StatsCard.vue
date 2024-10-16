7c248ddedb4b724647af6095cc8658ba5cd1622f
The Hoang <thehoang1722@gmail.com>
2024-10-16 22:09:12 +0700

    push code

---
+++ client/src/components/streamer/analytics/StatsCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+  const props = defineProps({
+    title: String,
+    value: [String, Number],
+  });
+</script>
+
+<template>
+  <div class="bg-white shadow-lg p-6 rounded-md">
+    <span class="text-base font-bold">{{ title }}</span>
+    <div class="text-[32px] font-bold">{{ value }}</div>
+  </div>
+</template>
---
+++ client/src/components/streamer/analytics/VideoAnalytics.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>dddđ</div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
