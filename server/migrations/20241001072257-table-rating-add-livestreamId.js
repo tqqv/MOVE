d03997b38bdafbd990a8940bc13b09a2c45d7709
tqqv <trinhquang96vu@gmail.com>
2024-10-21 11:36:33 +0700

    Update db: table rating field livestreamId allowNull is true

--- server/migrations/20241001072257-table-rating-add-livestreamId.js
+++ server/migrations/20241001072257-table-rating-add-livestreamId.js
@@ -5,7 +5,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn('ratings', 'livestreamId', {
       type: Sequelize.UUID,
-      allowNull: false,
+      allowNull: true,
       references: {
         model: 'livestreams', // Tên bảng trong cơ sở dữ liệu
         key: 'id',            // Tên cột bạn muốn tham chiếu đến
