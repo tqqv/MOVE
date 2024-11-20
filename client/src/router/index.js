d7112be90611de3a4825e1c268991174235b8354
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-11-20 15:43:49 +0700

    fix watch also

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -119,6 +119,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 export default router;
