8f7d9710752d322d25eba9a7419eea89aadd0bda
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-11-24 20:18:57 +0700

    fix scroll page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -119,9 +119,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior() {
-    return { top: 0 };
-  },
 });
 
 export default router;
