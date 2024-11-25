3cfd829d10300ae5a1a4bf50f2de790d207d6554
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2024-11-25 12:44:57 +0700

    fix in live

--- client/src/components/streamer/liveStream/InLiveStream.vue
+++ client/src/components/streamer/liveStream/InLiveStream.vue
@@ -5,6 +5,8 @@
   import InformationLiveStream from './InformationLiveStream.vue';
   import NotConnectScreen from './NotConnectScreen.vue';
   import { useStreamerStore, useUserStore } from '@/stores';
+  import ChartLive from './ChartLive.vue';
+  import TopDonates from './TopDonates.vue';
   const props = defineProps({
     liveStatus: String,
     connectOBS: String,
@@ -18,20 +20,7 @@
   const liveStreamData = computed(() => streamerStore?.streamerChannel?.id);
 </script>
 <template>
-  <div class="px-10 flex items-center flex-col">
-    <div class="flex w-full justify-center mb-4 max-w-[1660px]">
-      <div
-        class="flex flex-col p-4 rounded-md basis-full justify-center shadow-md bg-white overflow-hidden max-w-full"
-      >
-        <div class="flex justify-center gap-x-16 py-4">
-          <InformationLiveStream
-            :elapsedTime="elapsedTime"
-            :metricsData="metricsData"
-            :liveStatus="liveStatus"
-          />
-        </div>
-      </div>
-    </div>
+  <div class="px-10 flex items-center flex-col gap-y-7">
     <div class="w-full flex gap-x-8 justify-center">
       <div
         class="flex flex-col max-w-[1284px] min-w-[700px] rounded-md basis-full justify-center shadow-md bg-white overflow-hidden"
@@ -64,6 +53,28 @@
       </div>
       <!-- CHAT -->
       <LiveChat :liveStreamData="liveStreamData" :isStreamer="true" />
+    </div>
+    <div class="w-full flex gap-x-8 justify-center">
+      <!-- Information -->
+      <div class="flex max-w-[700px] w-[580px] flex-col basis-2/5">
+        <div class="bg-white rounded-lg shadow-md p-4 overflow-hidden">
+          <h1 class="font-semibold mb-4">Insights</h1>
+          <div class="grid grid-cols-2 gap-y-8 gap-x-16 mb-4 items-start ml-3">
+            <InformationLiveStream
+              :elapsedTime="elapsedTime"
+              :metricsData="metricsData"
+              :liveStatus="liveStatus"
+            />
+          </div>
+        </div>
+      </div>
+      <!-- Chart -->
+      <div class="flex max-w-[896px] w-[700px] max-h-[500px] flex-col basis-3/5">
+        <div class="bg-white rounded-lg shadow-md p-4 overflow-hidden">
+          <h1 class="font-semibold mb-4">Chart</h1>
+          <!-- <ChartLive /> -->
+        </div>
+      </div>
     </div>
   </div>
 </template>
