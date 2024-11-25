3d8994c14db3d350edb5b67cc270e552cdfc8059
tqqv <trinhquang96vu@gmail.com>
2024-11-25 11:10:12 +0700

    Update table users: Add fields nationality

---
+++ server/migrations/20241125033421-table-users-addFields-nationality.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('users', 'nationality', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('users', 'nationality');
+  },
+};
