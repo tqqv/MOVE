5b1bd8cf17c87b42a84aabef37c4c5e970575eda
linhne94 <linhnqde160572@fpt.edu.vn>
2024-11-30 11:40:46 +0700

    fixbug rating and edit profile

--- server/services/ratingService.js
+++ server/services/ratingService.js
@@ -100,7 +100,7 @@ const createRatingOnStream = async(userId, livestreamId, rating) => {
       }
     }
 
-    if(rating > 5 && rating < 0){
+    if(rating > 5 || rating < 0){
       return {
         status: 400,
         data: null,
