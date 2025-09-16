b1b1a9de3c65ac80dbf1fcdd48eedc731ef4d47a
tqqv <victortrnh@gmail.com>
2025-09-16 21:24:22 +0700

    update redis connect

--- server/utils/redis/config.js
+++ server/utils/redis/config.js
@@ -1,11 +1,20 @@
-// redis/config.js
 const Redis = require('ioredis');
 
 const redisConfig = {
-    host: process.env.REDIS_HOST,   // Địa chỉ Redis container trên Docker Desktop
-    port: 6379
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined, // nếu không đặt pass thì để undefined
+  tls: process.env.REDIS_TLS === "true" ? {} : undefined // nếu sau này dùng AWS ElastiCache có TLS
 };
 
 const _redis = new Redis(redisConfig);
+
+_redis.on('connect', () => {
+  console.log('✅ Connected to Redis');
+});
+
+_redis.on('error', (err) => {
+  console.error('❌ Redis error:', err);
+});
 
 module.exports = _redis;
