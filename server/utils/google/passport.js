fbbc20b61eba1c5744fc6768b19fcb423fe933d6
tqqv <victortrnh@gmail.com>
2025-09-21 06:18:02 +0700

    minor bug login gg be

--- server/utils/google/passport.js
+++ server/utils/google/passport.js
@@ -24,7 +24,7 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: 'https://move-c40k.onrender.com/api/auth/google/callback'
   },
   async (acceessToken, refreshToken, profile, done) => {
 
